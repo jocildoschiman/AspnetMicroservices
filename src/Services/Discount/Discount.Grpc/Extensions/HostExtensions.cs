3d3ee86076751edd3985bf8c09232cd90cc20264
Jocildo Schimanscky <schimanscky@hotmail.com>
2024-04-19 13:07:38 +0100

    Writing Grpc Module

---
+++ src/Services/Discount/Discount.Grpc/Extensions/HostExtensions.cs
@@ -0,0 +1,69 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System.Data;
+
+namespace Discount.Grpc.Extensions
+{
+    public static class HostExtensions
+    {
+        /// <summary>
+        /// Faz a vez do SeedData (carrega algumas informações iniciais e complementares no banco de dados antes do arranque da API)
+        /// </summary>
+        /// <typeparam name="TContext"></typeparam>
+        /// <param name="host"></param>
+        /// <param name="retry"></param>
+        /// <returns></returns>
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var configuration = services.GetRequiredService<IConfiguration>();
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    logger.LogInformation("Migrating PostgreSQL database");
+                    using(var conn = CreateConnection(configuration))
+                    {
+                        //Exclui a tabela se existir:
+                        var sql = "DROP TABLE IF EXISTS Coupon";
+                        conn.Execute(sql);
+                        //Cria uma nova tabela:
+                        sql = @"CREATE TABLE Coupon (Id SERIAL PRIMARY KEY, ProductName VARCHAR(24) NOT NULL, 
+                                    Description TEXT, Amount Double Precision);";
+                        conn.Execute(sql);
+                        //Insere alguns dados na tabela criada:
+                        sql = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('iPhone X','iPhone X Discount', 35000);";
+                        conn.Execute(sql);
+                        sql = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung Galaxy 23Ultra','Samsung Galaxy 23Ultra Discount', 99000);";
+                        conn.Execute(sql);
+
+                        logger.LogInformation("Migrated PostgreeSQL database");
+                    }
+
+                }
+                catch (NpgsqlException ex)
+                {
+                    logger.LogError(ex, "An error occurred while migrating the PostgreSQL database");
+                    if (retryForAvailability < 50)
+                    {
+                        retryForAvailability++;
+                        System.Threading.Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, retryForAvailability);
+                    }
+                }
+                return host;
+            }
+        }
+        private static IDbConnection CreateConnection(IConfiguration requiredConfiguration)
+        {
+            return new NpgsqlConnection(requiredConfiguration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Repositories/DiscountRepository.cs
@@ -0,0 +1,86 @@
+﻿using Dapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Repositories;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        /// <summary>
+        /// Injecção do IConfiguration para extrair a conexão do banco de dados
+        /// </summary>
+        private readonly IConfiguration _configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+        //Helpers:
+        /// <summary>
+        /// Fabrica a conexão através do IDbConnection (recebendo como parámetro a conexão do PostgreSQL) armazenada nas configurações da API (AppSettings.json)
+        /// </summary>
+        /// <returns></returns>
+        private IDbConnection CreateConnection()
+        {
+            return new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        }
+        /// <summary>
+        /// Retorna os dados de um cupon/desconto com base no produto informado e em caso de nulo, cria um novo cupon com desconto zerado
+        /// </summary>
+        /// <param name="productName"></param>
+        /// <returns></returns>
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            using(var conn = CreateConnection())
+            {
+                var sql = "SELECT * FROM Coupon WHERE ProductName=@Id";
+                return await conn.QueryFirstOrDefaultAsync<Coupon>(sql, new { Id = productName })
+                    ?? new Coupon { Description = "No Discount Desc", Amount = 0, ProductName = "No Discount" };
+            }
+        }
+        /// <summary>
+        /// Cria e salva um novo cupon no banco de dados e retorna como verdadeiro em caso de inserção com sucesso!
+        /// </summary>
+        /// <param name="coupon"></param>
+        /// <returns></returns>
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            using(var conn= CreateConnection())
+            {
+                var sql = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES(@ProductName, @Description, @Amount)";
+                return await conn.ExecuteAsync(sql, coupon) > 0;
+            }
+        }
+        /// <summary>
+        /// Actualiza os dados de um coupon no banco de dados
+        /// </summary>
+        /// <param name="coupon"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            using (var conn = CreateConnection())
+            {
+                var sql = "UPDATE Coupon SET ProductName=@ProductName, Description=@Description, Amount=@Amount WHERE Id=@Id";
+                return await conn.ExecuteAsync(sql, coupon) > 0;
+            }
+        }
+        /// <summary>
+        /// Exclui um coupon do banco de dados com base no ID (ProductName) informado
+        /// </summary>
+        /// <param name="productName"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            using (var conn = CreateConnection())
+            {
+                var sql = "DELETE FROM Coupon WHERE ProductName=@Id";
+                return await conn.ExecuteAsync(sql, new { Id = productName }) > 0;
+            }
+        }
+    }
+}
